bc4c9ede2661ca987a30a7c95cd6e4f679284a47
Ilyas <105958778+IlyasHere@users.noreply.github.com>
2023-04-18 13:41:21 +0700

    bebas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 
-
 import './App.css';
 import { Routes,  Route} from "react-router-dom";
 import Login from './login/login';
 import Register from "./register/register";
 import Home from "./pages/home";
 
-
-function App() {
+function App() {  
   return (
     <div>
       <Routes>  
