022c9f23b99174ea80524936b02b785e2d731acc
Ilyas <105958778+IlyasHere@users.noreply.github.com>
2023-04-26 17:05:20 +0700

    aapa

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Routes,  Route} from "react-router-dom";
 import Login from './login/login';
 import Register from "./register/register";
 import Home from "./pages/home";
+import Whislist from './whislist/whislist';
 
 function App() {  
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/" element={<Home />} />
+        <Route path="/whislist" element={<Whislist />} />
         </Routes>
     </div>
   );
---
+++ src/whislist/whislist.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "../component/navbar/navbar";
+import Footer from "../component/footer/footer";
+import { Link } from 'react-router-dom';
+
+
+function Whislist() {  
+    return (
+     <div>
+      <Navbar />
+     </div>
+    
+     
+    );
+  }
+  
+  export default Whislist;
+  
