f53a769ac9df1e37ecc4c7ae3a89c9321b4fb2c5
“[Amri Iqra Samudra Al-Fatihah <amriiqosamudraalfatihah@gmail.com>
2023-05-13 11:13:35 +0700

    feat: [AI] chat in toko

--- src/component/navbar/navbarToko.jsx
+++ src/component/navbar/navbarToko.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import IconNotif from "../../assets/icon/notif.svg";
 import logoBelanjaID from "../../assets/logoIMG/logo belanjaid.svg";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "../../style/navbarToko.css";
 import axios from "axios";
 import apiurl from "../../utils/apiurl";
@@ -9,6 +9,7 @@ import { useEffect } from "react";
 import { useState } from "react";
 
 function NavbarToko() {
+  const navigate = useNavigate();
   const [profile, setProfile] = useState({});
   const token = localStorage.getItem("token");
   async function getprofile() {
@@ -22,6 +23,21 @@ function NavbarToko() {
       .then((response) => {
         console.log(response);
         setProfile(response.data.data);
+      })
+      .catch((error) => {});
+  }
+
+  async function logout() {
+    await axios({
+      method: "post",
+      url: apiurl() + "logout",
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    })
+      .then((response) => {
+        console.log(response);
+        navigate("/login");
       })
       .catch((error) => {});
   }
@@ -44,13 +60,30 @@ function NavbarToko() {
           </div>
           <div className="line-toko"></div>
           {token ? (
-            <div className="myshop">
+            <div className="drop-profile">
               <img
                 className="photo-profile"
                 src={profile.profile_photo_url}
                 alt=""
               />
-              <h3 className="text-toko">{profile.name}</h3>
+              <div className="menu-dropdown">
+                <div className="user-info">
+                  <img
+                    className="photo-profile"
+                    src={profile.profile_photo_url}
+                    alt=""
+                  />
+                  <h3 className="nama-user">{profile.name}</h3>
+                </div>
+                <hr />
+                <a href="/" className="sub-menu-link">
+                  Profile
+                </a>
+                <a href="Whislist">Whislist</a>
+                <button className="btn-logout" onClick={logout}>
+                  Logout
+                </button>
+              </div>
             </div>
           ) : (
             <div className="login-daftar">
--- src/pages/daftarProduk.jsx
+++ src/pages/daftarProduk.jsx
@@ -1,9 +1,97 @@
-import React from 'react'
+import React from "react";
+import "../style/daftarproduk.css";
 
 function DaftarProduk() {
+  const data = [
+    {
+      name: "Anom",
+      age: 19,
+      gender: "Male",
+      imgProduk: "https://mg.co.id/wp-content/uploads/2020/03/nr20_yel_bk.jpg",
+    },
+    {
+      name: "Megha",
+      age: 19,
+      gender: "Female",
+      imgProduk: "https://mg.co.id/wp-content/uploads/2020/03/nr20_yel_bk.jpg",
+    },
+    {
+      name: "Subham",
+      age: 25,
+      gender: "Male",
+      imgProduk: "https://mg.co.id/wp-content/uploads/2020/03/nr20_yel_bk.jpg",
+    },
+  ];
+
   return (
-    <div>DaftarProduk</div>
-  )
+    <div className="container-daftarProduk">
+      <h1>Daftar Produk</h1>
+      <div className="btn-tambahProduk">
+        <button>Tambah Produk</button>
+      </div>
+      <div className="table-daftarProduk">
+        <div className="top-daftarProduk">
+          <div className="tab-daftar">
+            <h1>Semua Produk(1)</h1>
+          </div>
+        </div>
+        <div className="filter-section">
+          <div className="search-daftar">
+            <input type="text" placeholder="Cari Produk" />
+          </div>
+          <div className="dropdown-produk">
+            <select name="kategori-produk" id="kategori-produk">
+              <option value="kategori-produk">Kategori Produk</option>
+              <option value="kategori-produk">Kategori Produk</option>
+              <option value="kategori-produk">Kategori Produk</option>
+              <option value="kategori-produk">Kategori Produk</option>
+            </select>
+          </div>
+          <div className="dropdown-produk">
+            <select name="filter-produk" id="filter-produk">
+              <option value="kategori-produk">Filter</option>
+              <option value="kategori-produk">Kategori Produk</option>
+              <option value="kategori-produk">Kategori Produk</option>
+              <option value="kategori-produk">Kategori Produk</option>
+            </select>
+          </div>
+          <div className="dropdown-produk">
+            <select name="urutkan-produk" id="urutkan-produk">
+              <option value="kategori-produk">Urutkan</option>
+              <option value="kategori-produk">Kategori Produk</option>
+              <option value="kategori-produk">Kategori Produk</option>
+              <option value="kategori-produk">Kategori Produk</option>
+            </select>
+          </div>
+        </div>
+        <div className="table-produk">
+          <table>
+            <tr>
+              <th>Info Produk</th>
+              <th>Harga</th>
+              <th>Stok</th>
+              <th>Aksi</th>
+            </tr>
+            {data.map((val, key) => {
+              return (
+                <tr key={key}>
+                  <td>
+                    <img src={val.imgProduk} alt="" width={100} />
+                  </td>
+                  <td>{val.name}</td>
+                  <td>{val.age}</td>
+                  <td>
+                    <button className="btn-edit">Edit</button>
+                    <button className="btn-hapus">Hapus</button>
+                  </td>
+                </tr>
+              );
+            })}
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default DaftarProduk
+export default DaftarProduk;
